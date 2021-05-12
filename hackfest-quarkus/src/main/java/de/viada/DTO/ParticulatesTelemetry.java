f83d3422c18ff7e06ec111ead42633313a67d5c1
Dorra <dorra.laouiti@esprit.tn>
2021-05-12 18:42:19 +0200

    TOJSON funtion for Particulates

--- hackfest-quarkus/src/main/java/de/viada/DTO/ParticulatesTelemetry.java
+++ hackfest-quarkus/src/main/java/de/viada/DTO/ParticulatesTelemetry.java
@@ -16,7 +16,25 @@ public class ParticulatesTelemetry {
         particulatesDTO = _particulatesDTO;
         stationID = _stationID;
     }
-
+    public String toJSON() {
+        return "{" +
+                "\"stationID\":" + stationID +
+                ",\"instant\": \"" + instant + "\"" +
+                ",\"pm1_0\":" + particulatesDTO.getPm1_0()+
+                ",\"pm2_5\":" + particulatesDTO.getPm2_5() +
+                ",\"pm10\":" + particulatesDTO.getPm10()+
+                ",\"pm1_0_atm\":" + particulatesDTO.getPm1_0_atm() +
+                ",\"pm2_5_atm\":" + particulatesDTO.getPm2_5_atm()+
+                ",\"pm10_atm\":" + particulatesDTO.getPm10_atm() +
+                ",\"gt0_3um\":" + particulatesDTO.getGt0_3um()+
+                ",\"gt0_5um\":" + particulatesDTO.getGt0_5um()+
+                ",\"gt1_0um\":" + particulatesDTO.getGt1_0um()+
+                ",\"gt2_5um\":" + particulatesDTO.getGt2_5um() +
+                ",\"gt5_0um\":" + particulatesDTO.getGt0_5um() +
+                ",\"gt10um\":" + particulatesDTO.getGt10um() +
+                ",\"status\":" + particulatesDTO.getStatus() +
+                "}";
+    }
     public String getInstant() { return instant; }
 
     public void setInstant(String instant) { this.instant = instant; }
