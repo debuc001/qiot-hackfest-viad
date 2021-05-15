be3b37dc2a45aeafb0d8a8b8f0f9ff5e6e396d17
patplu <plum@viada.de>
2021-05-15 12:40:35 +0200

    - added application.properties - deleted the tests

--- hackfest-quarkus/src/test/java/com/sample/NativeQuarkusEndpointIT.java
+++
@@ -1,9 +0,0 @@
-package com.sample;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeQuarkusEndpointIT extends QuarkusEndpointTest {
-
-    // Execute the same tests but in native mode.
-}
